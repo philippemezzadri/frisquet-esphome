1b0e274f8f7594b67bdedadff2a0f1dabd76af3e
Philippe Mezzadri <48350922+philippemezzadri@users.noreply.github.com>
2024-11-15 12:25:58 +0100

    Update frisquet_boiler.cpp

--- components/frisquet_boiler/frisquet_boiler.cpp
+++ components/frisquet_boiler/frisquet_boiler.cpp
@@ -121,7 +121,7 @@ void FrisquetBoiler::send_message() {
  * @param byteValue Byte value to be serialized
  * @param byteIndex Order of the byte in the message, used for bit stuffing
  */
-void FrisquetBoiler::serialize_byte(uint8_t byteValue, uint8_t byteIndex, uint8_t msgSize) {
+void FrisquetBoiler::serialize_byte(uint8_t byteValue, uint8_t byteIndex, uint8_t msgSize = 14) {
   for (uint8_t n = 0; n < 8; n++) {
     int bitValue = ((byteValue >> n) & 0x1);  // bitread
     this->write_bit(bitValue);
